f6de8d32c5536d5140a5c824a32d84fe4eb3c48a
Yuki Yoshida <yshdyk11202000@gmail.com>
2021-12-06 08:29:25 +0900

    cdk init commit

---
+++ test/cdk-apigateway-lambda-express.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as CdkApigatewayLambdaExpress from '../lib/cdk-apigateway-lambda-express-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-apigateway-lambda-express-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkApigatewayLambdaExpress.CdkApigatewayLambdaExpressStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
